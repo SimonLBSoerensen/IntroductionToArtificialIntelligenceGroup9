cd1928be6d9cd808dc7b58e3ee0f46e98a90500d
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2019-09-12 16:04:19 +0200

    code

--- code/test.py
+++ code/test.py
@@ -1,9 +1,24 @@
 import ev3dev2
-from ev3dev2 import motor
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+
 from ev3dev2 import sensor
 from ev3dev2 import display
 from ev3dev2.sensor import lego
-infrared_sensor = lego.InfraredSensor(sensor.INPUT_4)
+
+
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+
+# drive in a turn for 5 rotations of the outer motor
+# the first two parameters can be unit classes or percentages.
+tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(75), 10)
+
+# drive in a different turn for 3 seconds
+tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
+
+infrared_sensor = lego.InfraredSensor(INPUT_4)
 
 disp = display.Display()
 

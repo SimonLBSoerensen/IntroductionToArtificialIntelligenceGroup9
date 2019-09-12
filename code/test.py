c1aba53490619a44ef4c8c44ac097a1328cb9a19
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2019-09-12 15:59:33 +0200

    code

--- code/test.py
+++ code/test.py
@@ -1,29 +1,13 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, MoveTank, SpeedPercent
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
-from ev3dev2.sound import Sound
-import time
-sound = Sound()
+import ev3dev2
+from ev3dev2 import motor
+from ev3dev2 import sensor
+from ev3dev2 import display
+from ev3dev2.sensor import lego
+infrared_sensor = lego.InfraredSensor(sensor.INPUT_4)
 
-sound.speak("I am ready in 3")
-time.sleep(1)
-sound.speak("2")
-time.sleep(1)
-sound.speak("1")
-time.sleep(1)
-sound.speak("Lift off")
+disp = display.Display()
 
-tank = MoveTank(OUTPUT_A, OUTPUT_D)
+while True:
+    dist = infrared_sensor.distance()
+    disp.text_grid(dist)
 
-for speed_pro in range(20, 100, 20):
-    speed_pro = -1*speed_pro
-
-    tank.on(SpeedPercent(speed_pro), SpeedPercent(speed_pro))
-    time.sleep(0.2)
-
-sound.speak("Top speed")
-time.sleep(3)
-tank.on(0,0)
-
-sound.speak("mission completed")

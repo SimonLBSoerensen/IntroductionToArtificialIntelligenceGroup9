77046bdfc31ebd85a7a25c4f00a4a42ff12b5200
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2019-09-16 18:49:34 +0200

    jokes

--- code/jokes/joke_test.py
+++ code/jokes/joke_test.py
@@ -83,7 +83,6 @@ def play_joke(joke_group=None):
             sound_file = random.choice(files)
         else:
             sound_file = base_folder + "/sounds/" + sound_file
-        print(sound_file)
         ext = sound_file.split(".")[-1]
 
         if ext != "wav":

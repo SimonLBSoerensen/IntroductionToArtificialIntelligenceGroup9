0b5a4d462f140b54f4b48fd14756802de2b9edc2
SimonLBSoerensen <simonlyckbjaert@hotmail.com>
2019-12-16 15:22:13 +0100

    backing

--- code/a_new_new_start.py
+++ code/a_new_new_start.py
@@ -233,7 +233,7 @@ states = [
 
 states_string = "F2, T, F2, T, F1, F3, L, F1, L, F4, L, F2, R, F1, R, F1, R, F4, F1, T, F4, F1, L, F1, T, F1, T, F1, T, F1, R, F5, L, F1, L, F3, F1, L, F3, F1, T, F1, F1, F1, F1, T, F2, R, F1, T, F1, R, F1, F1, T, F3, F1, R, F1, F3, T, F1, F3, L, F4, T, F2, L, F1, T, F1, L, F2, T, F4, T, F2, F1, L, F1, T, F2, F1, L, F1, R, F2, R, F1, T, F1, L, F2, F1, F1, T, F2, F2, R, F2, R, F1, F1, T, F1, F1, T, F2, R, F1, L, F2, F1, T, F4, F1, T, F2, F3, L, F1, L, F1, L, F2, R, F2, R, F1, R, F3, L, F1, T, F1, R, F4, L, F1, R, F1, T, F2, L, F1, F1, T, F1, T, F1, T, F2, R, F1, F1, T, F1, F3, L, F4, R, F4, T, F3, T, F1, F2, R, F2, R, F1, L, F1, T, F3, F1, L, F1, L, F1, L, F1, L, F1, L, F1, L, F1, T, F1, T, F1, L, F1, T, F2, F1, T, F3, L,"
 #states_string = "F1, L, P, F2, R, P, F3, L, P, F3, L, P, F3, R, P, F3,"
-states_string = "F1, R, F1, R, F1, R, F1, R, F1, R, F1, L, F1, L, F1, L, F1, L, F1, T, O,"
+states_string = "F1, R, F1, R, F1, R, F1, R, F1, L, F1, L, F1, L, F1, L, F1, L, F1, T, O,"
 
 states = []
 temp = None
